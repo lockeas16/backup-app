2c546568b64aca976b5acd4be6d158b0b5b3bcb0
rpernicone <roberto.pernicone@gmail.com>
2019-04-26 19:33:10 -0500

    nothing done

--- public/javascripts/zoneForm.js
+++ public/javascripts/zoneForm.js
@@ -1,6 +1,6 @@
 // zoneId and coordinates were previously created through Handle Bars
 // TODO: change baseUrl for heroku!
-const zoneHandler = new ZoneHandler(baseUrl);
+const zoneHandler = new ZoneHandler();
 let btn = document.getElementById("zoneBtn");
 btn.disabled = true;
 mapboxgl.accessToken =
