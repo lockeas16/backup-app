209c065921598162bb63f551751ba6abe444f474
lockeas16 <lockeas16@gmail.com>
2019-04-30 20:24:30 -0500

    player showing first images directly from campaigns

--- routes/player.js
+++ routes/player.js
@@ -1,12 +1,13 @@
-const express       = require('express');
-const server        = require('http').Server(express);
-const io            = require('socket.io')(server)
-const Campaign      = require('../models/Campaign');
-const playControler = require('../helpers/bidPlayer')
-const router        = express.Router();
+const express = require("express");
+const server = require("http").Server(express);
+const io = require("socket.io")(server);
+const Campaign = require("../models/Campaign");
+const playControler = require("../helpers/bidPlayer");
+const router = express.Router();
+const Zone = require("../models/Zone");
 
-router.get('/', (req, res) =>{
-  res.render('player', {layout: false})
-})
+router.get("/", (req, res) => {
+  res.render("player", { layout: false });
+});
 
 module.exports = router;
